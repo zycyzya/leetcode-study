6aa9304fe181c511c47d517b5dd854f837f7c354
zycyzya <1660193483@qq.com>
2024-12-20 13:17:40 +0800

    更新readme

--- leetcode 1两数之和.cpp
+++ leetcode 1两数之和.cpp
@@ -4,8 +4,8 @@ int sum(int *nums,int size,int target){
 	for(int i=0;i<size;i++){
 		for(int j=i+1;j<size;j++){
 			if(nums[i]+nums[j]==target){
-				res[0]=i;
-				res[1]=j;	
+				res[0]=i+1;
+				res[1]=j+1;	
 				break;
 			}
 		}
